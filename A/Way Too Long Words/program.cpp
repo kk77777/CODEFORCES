738301fac2021f0e2a8e5203eeb93e4c559ae610
kk77777 <kaushikgattani@gmail.com>
2020-01-26 05:28:46 +0530

    Solution for (A) Way Too Long Words

---
+++ A/Way Too Long Words/program.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        if (s.length() <= 10)
+        {
+            cout << s << "\n";
+        }
+        else
+        {
+            cout << s[0];
+            cout << s.length() - 2;
+            cout << s.back();
+            cout << "\n";
+        }
+    }
+#ifndef ONLINE_JUDGE
+    cout << "\nTime Elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC << " s\n";
+#endif
+    return 0;
+}
